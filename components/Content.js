a616c6fa2e78e0bf045010e7a7ff86a9c452fe52
MrPotato98 <huy.potato1998@gmail.com>
2019-12-13 16:52:37 +0700

    countdown

--- components/Content.js
+++ components/Content.js
@@ -2,13 +2,68 @@ import React from 'react';
 import {View, Text, Dimensions, StyleSheet} from 'react-native';
 const {width, height} = Dimensions.get('window');
 export default class Content extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: {},
+      now: new Date().toDateString(),
+      coutdown: new Date('Jan 25, 2020 15:37:25').toDateString(),
+    };
+    this.timer = 0;
+  }
+  secondsToTime(secs) {
+    // let now = this.state.now;
+    let days = Math.floor(secs / (60 * 60 * 24));
+    let divisor_for_hours = secs % (60 * 60 * 24);
+    let hours = Math.floor(divisor_for_hours / (60 * 60));
+
+    let divisor_for_minutes = secs % (60 * 60);
+    let minutes = Math.floor(divisor_for_minutes / 60);
+
+    let divisor_for_seconds = divisor_for_minutes % 60;
+    let seconds = Math.ceil(divisor_for_seconds);
+
+    let obj = {
+      // n: now,
+      d: (days < 10 ? '0' : '') + days,
+      h: (hours < 10 ? '0' : '') + hours,
+      m: (minutes < 10 ? '0' : '') + minutes,
+      s: (seconds < 10 ? '0' : '') + seconds,
+    };
+    return obj;
+  }
+  componentDidMount() {
+    // let now = this.secondsToTime(this.state.now);
+    this.clockCall = setInterval(() => {
+      this.countDown();
+      //   this.decrementClock();
+    }, 1000);
+  }
+  countDown = () => {
+    // Remove one second, set state so a re-render happens.
+    // if (this.state.timer === 0) clearInterval(this.clockCall);
+    // this.timer = setInterval(this.countDown, 1000);
+    let now = new Date().getTime();
+    let time = new Date('Jan 25, 2020 15:37:25').getTime();
+
+    let seconds = time - now;
+    console.log(seconds);
+
+    this.setState({
+      time: this.secondsToTime(seconds / 1000),
+      seconds: seconds,
+    });
+    if (seconds == 0) {
+      clearInterval(this.timer);
+    }
+  };
   render() {
     return (
       <View style={CSS.view1}>
         <View style={CSS.container}>
           <View style={CSS.container}>
             <Text style={CSS.test1}>
-              HÔM NAY: Thứ sáu ngày 13/12/2019 (Dương lịch) | 18/11/2019 (Âm
+              HÔM NAY: {this.state.now} (Dương lịch) | {this.state.coutdown} (Âm
               lịch)
             </Text>
           </View>
@@ -21,7 +76,7 @@ export default class Content extends React.Component {
 
         <View style={CSS.container}>
           <View style={CSS.container}>
-            <Text style={CSS.test3}>42</Text>
+            <Text style={CSS.test3}>{this.state.time.d}</Text>
           </View>
           <View style={CSS.container}>
             <Text style={CSS.test4}>NGÀY</Text>
@@ -29,15 +84,15 @@ export default class Content extends React.Component {
         </View>
         <View style={CSS.container1}>
           <View style={CSS.container}>
-            <Text style={CSS.test2}>09</Text>
+            <Text style={CSS.test2}>{this.state.time.h}</Text>
             <Text style={CSS.test2}>Giờ</Text>
           </View>
           <View style={CSS.container}>
-            <Text style={CSS.test2}>09</Text>
+            <Text style={CSS.test2}>{this.state.time.m}</Text>
             <Text style={CSS.test2}>Phút</Text>
           </View>
           <View style={CSS.container}>
-            <Text style={CSS.test2}>09</Text>
+            <Text style={CSS.test2}>{this.state.time.s}</Text>
             <Text style={CSS.test2}>Giây</Text>
           </View>
         </View>
